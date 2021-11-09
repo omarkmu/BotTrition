ced9b62cb4ba97ebdc1bbe392ce83bca8b4519cb
omarkmu <omarkmu@gmail.com>
2021-11-09 12:50:46 -0500

    feat: Added declarations for FDC API functions docs: Added documentation for API arguments

--- fdc.py
+++ fdc.py
@@ -47,10 +47,82 @@ def _post(endpoint, data=None):
     return response
 
 
+# setting up initial declarations without bodies
+# while deciding how to structure responses.
+
+# ultimately, these will probably return an
+# object instead of dict so it's easier to work with the results
+
+
+def get_food_by_id(fdc_id, nutrient_ids=None, abridged=False):
+    """
+    Retrieves information about a food given its FDC ID.
+
+    Args:
+        - fdc_id: The FDC ID of the food to retrieve.
+        - nutrient_ids: An optional list of up to 25 nutrients to retrieve.
+            If the food has no matching with the supplied nutrients,
+            the "foodNutrients" field in the response will be empty.
+        - abridged: Whether to return abridged results.
+    """
+
+
+def get_foods(fdc_ids, nutrient_ids=None, abridged=False):
+    """
+    Retrieves information about multiple foods given their FDC IDs.
+
+    Args:
+        - fdc_ids: A list of FDC IDs for foods to retrieve.
+        - nutrient_ids: An optional list of up to 25 nutrients to retrieve.
+            If a food has no matching with the supplied nutrients,
+            the "foodNutrients" field will be empty.
+        - abridged: Whether to return abridged results.
+    """
+
+
+# the "dataType" parameter is intentionally excluded from this function
+# and from the search function as a design decision; the "Foundation" and "Branded"
+# types are the only typed which include food nutrients, which
+# are an essential part of our app, so those two types will be
+# the only acceptable data types, and they will always be used as filters.
+
+
+def list_foods(page=1, per_page=50, sort=None, sort_direction=None):
+    """
+    Retrieves a list of available foods.
+
+    Args:
+        - page: The page at which the list should start.
+        - per_page: The number of results per page.
+        - sort: The field to sort by;one of "dataType",
+            "description", "fdcId", or "publishedDate".
+        - sort_direction: The direction that the sort
+            should be applied; "asc" or "desc".
+    """
+
+
+def search(query, page=1, per_page=50, sort=None, sort_direction=None):
+    """
+    Searches for foods based on a search query.
+
+    Args:
+        - query: The search query.
+        - page: The page at which the search results should start.
+        - per_page: The number of results per page.
+        - sort: The field to sort by; one of "dataType",
+            "description", "fdcId", or "publishedDate".
+        - sort_direction: The direction that the sort
+            should be applied; "asc" or "desc".
+    """
+
+
 def set_key(key):
     """
     Sets the FoodData Central API key
     to be used during API requests.
+
+    Args:
+        - key: The FDC API key to use.
     """
 
     _SECRETS["FDC_KEY"] = key
--- fdc.pyi
+++ fdc.pyi
@@ -1,1 +1,22 @@
+from typing import List, Literal
+
+def get_food_by_id(
+    fdc_id: int, nutrient_ids: List[int] = None, abridged: bool = False
+) -> dict: ...
+def get_foods(
+    fdc_ids: List[int], nutrient_ids: List[int] = None, abridged: bool = False
+) -> dict: ...
+def list_foods(
+    page: int = 1,
+    per_page: int = 50,
+    sort: Literal["dataType", "description", "fdcId", "publishedDate"] = None,
+    sort_direction: Literal["asc", "desc"] = None,
+) -> list: ...
+def search(
+    query: str,
+    page: int = 1,
+    per_page: int = 50,
+    sort: Literal["dataType", "description", "fdcId", "publishedDate"] = None,
+    sort_direction: Literal["asc", "desc"] = None,
+): ...
 def set_key(key: str) -> None: ...
