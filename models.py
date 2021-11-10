8c9f837b9b14cff1da6c89ee8ab7e92b43f24927
sourav199 <souravdhar1999@gmail.com>
2021-11-10 15:49:27 -0500

    fix: Fixed some issues with renaming variable and few bugs.

--- models.py
+++ models.py
@@ -2,14 +2,14 @@ from sqlalchemy.orm import backref
 from app import db
 
 
-class owner(db.Model):
+class Btowner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
-    password = db.Column(db.String(80))
+    password_hash = db.Column(db.String(80))
     foodAllergies = db.relationship("Allergies", backref="food_allergies")
     dietaryrestriction = db.relationship("DietaryRestriction", backref="food_dietary")
     ownerage = db.relationship("Age", backref="user_age", uselist=False)
-    profileData = db.relationship("Age", backref="user_data", uselist=False)
+    profileData = db.relationship("Profile", backref="user_data", uselist=False)
 
 
 # So basically the Allergies and DietaryRestriction are multivalued so our user will have one to many relationship with these two attributes.
@@ -21,17 +21,17 @@ class Profile(db.Model):
     gender = db.Column(db.String(80))
     height = db.Column(db.Integer)
     weight = db.Column(db.Integer)
-    user_data = db.Column(db.Integer, db.ForeignKey("owner.id"))
+    user_data = db.Column(db.Integer, db.ForeignKey("btowner.id"))
 
 
 class Allergies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    users_food_allergies = db.Column(db.Integer, db.ForeignKey("owner.id"))
+    users_food_allergies = db.Column(db.Integer, db.ForeignKey("btowner.id"))
 
 
 class DietaryRestriction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    users_food_dietary = db.Column(db.Integer, db.ForeignKey("owner.id"))
+    users_food_dietary = db.Column(db.Integer, db.ForeignKey("btowner.id"))
 
 
 class Age(db.Model):
