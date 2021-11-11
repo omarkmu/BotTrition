390d59d793d542b418403dc149006c3a17d37f1b
omarkmu <omarkmu@gmail.com>
2021-11-11 14:32:16 -0500

    lint: Fixed linter warnings in python and js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useRef } from 'react';
-
+import React from 'react';
 
 function App() {
   // fetches JSON data passed in by flask.render_template and loaded
   // in public/index.html in the script with id "data"
-  const args = JSON.parse(document.getElementById("data").text);
+  // const args = JSON.parse(document.getElementById("data").text);
 
+  // This main page will be further designed with react components
+  // so the user can interact with the main app page. Some Data will also
+  // be present for viewing when user lands on this page.
   return (
     <h1> ! Welcome To Main App Page User !</h1>
-  
-    //This main page will be further designed with react components
-    //so the user can interact with the main app page. Some Data will also
-    //be present for viewing when user lands on this page.
-    );
+  );
 }
 
 export default App;
