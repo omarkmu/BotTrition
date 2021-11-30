408a79eaee5887da2e795614cb7f40f069f82848
Brandon Dwyer <bdwyer1@student.gsu.edu>
2021-11-30 08:17:50 +0000

    fix: minor changes to resolve merge conflicts

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import App from './pages/App';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Registration from './pages/Registration';
-import Landing from './pages/Landing';
 
 // reads information sent from flask and deletes the element containing it
 function getData(key, json = true) {
@@ -20,35 +20,12 @@ const flashes = getData('flashes');
 const form = getData('form_data');
 
 export default function AppRouter() {
-  // temporary solution; should be replaced with React Router
-  const path = window.location.pathname.slice(1); // remove the leading slash
-  let Page;
-  switch (path) {
-    case '':
-      Page = Landing;
-      break;
-    case 'app':
-      Page = App;
-      break;
-    case 'login':
-      Page = Login;
-      break;
-    case 'registration':
-      Page = Registration;
-      break;
-    case 'profile':
-      Page = Profile;
-      break;
-    default:
-      throw new Error('unreachable');
-  }
-
   return (
-    <Page
-      csrfToken={csrfToken}
-      data={data}
-      flashes={flashes}
-      form={form}
-    />
+    <Routes>
+      <Route path="app" element={<App />} />
+      <Route path="login" element={<Login csrfToken={csrfToken} flashes={flashes} form={form} />} />
+      <Route path="registration" element={<Registration csrfToken={csrfToken} flashes={flashes} form={form} />} />
+      <Route path="profile" element={<Profile csrfToken={csrfToken} data={data} flashes={flashes} form={form} />} />
+    </Routes>
   );
 }
