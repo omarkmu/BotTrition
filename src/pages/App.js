156a74106b4b05782076408cc5f635a2a9683b95
sourav199 <souravdhar1999@gmail.com>
2021-11-30 12:07:06 -0500

    fix: Added the unitName value

--- src/pages/App.js
+++ src/pages/App.js
@@ -80,7 +80,7 @@ export default function App() {
                       {element.nutrientName}
                       =
                       {element.value}
-                      g
+                      {element.unitName}
                     </p>
                   ))}
 
