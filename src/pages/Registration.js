82f6258df5b14348d0e44302a80c91f6239e44de
Drew Hartman <drewdavishartman@gmail.com>
2021-12-03 14:50:27 -0500

    feat: added header

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import {
   Container, Flashes, Form, Input, Row, Submit,
 } from '../Components';
+import Header from '../components/Header';
 
 export default function Registration(props) {
   const {
@@ -16,43 +17,46 @@ export default function Registration(props) {
   });
 
   return (
-    <Container>
-      <h1>Sign Up</h1>
+    <div>
+      <Header />
+      <Container>
+        <h1>Sign Up</h1>
 
-      <Row>
-        <Form token={csrfToken}>
-          <Input
-            form={form}
-            type="text"
-            id="username"
-            placeholder="Username"
-            minlength="4"
-            maxlength="20"
-            required
-          />
+        <Row>
+          <Form token={csrfToken}>
+            <Input
+              form={form}
+              type="text"
+              id="username"
+              placeholder="Username"
+              minlength="4"
+              maxlength="20"
+              required
+            />
 
-          <Input
-            form={form}
-            type="password"
-            id="password"
-            placeholder="Password"
-            minlength="4"
-            maxlength="20"
-            required
-          />
+            <Input
+              form={form}
+              type="password"
+              id="password"
+              placeholder="Password"
+              minlength="4"
+              maxlength="20"
+              required
+            />
 
-          <Submit id="submit" value="Register" />
-        </Form>
-      </Row>
+            <Submit id="submit" value="Register" />
+          </Form>
+        </Row>
 
-      <Flashes flashes={flashes} />
+        <Flashes flashes={flashes} />
 
-      <Row>
-        Already have an account? Log in
-        {' '}
-        <Link to="/login">here</Link>
-        .
-      </Row>
-    </Container>
+        <Row>
+          Already have an account? Log in
+          {' '}
+          <Link to="/login">here</Link>
+          .
+        </Row>
+      </Container>
+    </div>
   );
 }
